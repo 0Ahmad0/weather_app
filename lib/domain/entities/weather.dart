b9c2c3679df084419eebf008654b70607eec1cc6
Ahmad Alhariri‏ <0ahmad.haboosh0@gmail.com>
2023-04-16 05:37:51 +0300

    add entities folder and app api

---
+++ lib/domain/entities/weather.dart
@@ -0,0 +1,16 @@
+class Weather{
+  final int id;
+  final String cityName;
+  final String name;
+  final String description;
+  final int pressure;
+
+  Weather({
+   required this.id,
+   required this.cityName,
+   required this.name,
+   required this.description,
+   required this.pressure,
+  });
+
+}
